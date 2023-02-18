057890a9016d9326d47c532e0ffd1533ee784b7f
guoxin98 <1334074229@qq.com>
2023-02-18 22:50:30 +0800

    视频页完成

--- pages/home-video/index.js
+++ pages/home-video/index.js
@@ -10,22 +10,46 @@ Page({
   data: {
     topMVs:[],
     offset:0,
-    limit:10
+    limit:10,
+    hasMore:false,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.getTopMV()
+  },
+  getTopMV(){
     const obj = {
       offset:this.data.offset,
       limit:this.data.limit,
     }
     getTopMV(obj).then(res=>{
       this.setData({
-        topMVs:res.data.data
+        topMVs:this.data.topMVs.length?
+        this.data.offset===0?
+        res.data.data:this.data.topMVs.concat(res.data.data)
+        :res.data.data,
+        hasMore:res.data.hasMore
       })
+      wx.hideNavigationBarLoading()
+      if(this.data.offset===0){
+        wx.stopPullDownRefresh()
+      }
     })
+  },
+  // 点击事件
+  handleVideoItemClick(event){
+    const id = event.currentTarget.dataset.item.id //获取点击的视频的id
+   wx.navigateTo({
+      url: `/pages/detail-video/index?id=${id}`,
+      success: (result)=>{
+        
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
@@ -58,14 +82,26 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    this.setData({
+      offset:0
+    })
+    this.getTopMV()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom() {
-
+    if(this.data.hasMore){
+      this.setData({
+        offset:this.data.offset+this.data.limit
+      })
+      wx.showNavigationBarLoading()
+      this.getTopMV()
+    }
+    if(this.data.offset===0){
+      wx.startPullDownRefresh()
+    }
   },
 
   /**
