a7320710a31fa20996596a09eb6b75938ebd430a
guoxin98 <1334074229@qq.com>
2023-04-03 00:48:25 +0800

    area-header组件优化、新增删除搜索历史功能

--- pages/detail-search/index.js
+++ pages/detail-search/index.js
@@ -92,6 +92,12 @@ Page({
       showDropdown:false
     })
   },
+  clearSearchHistory(){
+    storage.remove(SEARCH_HISTORY)
+    this.setData({
+      searchHistory:[]
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
